b7203c14b8ad3283c6c473e9c262a5fe9ed526cb
Basil <i.najar96@gmail.com>
2024-08-07 16:48:00 -0400

    Add test .csv file and update app architecture

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+      <h2>Test2</h2>
+    </div>
+  );
+}
+
+export default App;
